6038eeceb8e11ba828c01a09873f2d791fbd892b
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2025-05-19 16:36:19 +0700

    adding cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
                             </a>
 
                             <a
-                                href=""
+                                href="https://drive.google.com/file/d/1nLmB4rh2Hrwr2uvVfq2Aoh2XZZoc2Ama/view?usp=sharing"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                             >
                                 Download CV
